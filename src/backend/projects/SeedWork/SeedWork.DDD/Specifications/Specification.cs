f948c5a7931839ce42761e03006ae04d5170ec47
Dmitriy Onypko <vaporif@gmail.com>
2022-07-25 14:41:58 +0100

    specifications

---
+++ src/backend/projects/SeedWork/SeedWork.DDD/Specifications/Specification.cs
@@ -0,0 +1,18 @@
+namespace SeedWork.DDD.Specifications;
+
+using System.Linq.Expressions;
+
+public abstract class Specification<T>
+{
+    public abstract Expression<Func<T, bool>> Expression { get; }
+
+    public bool IsSatisfiedBy(T entity)
+    {
+        var predicate = Expression.Compile();
+        return predicate(entity);
+    }
+
+    public Specification<T> And(Specification<T> specification) => new AndSpecification<T>(this, specification);
+
+    public Specification<T> AndNot(Specification<T> specification) => new AndNotSpecification<T>(this, specification);
+}
