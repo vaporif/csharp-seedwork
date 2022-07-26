92ef6f0f7fe91fc73bbdd1d2b45a33e3a9baf395
Dmitriy Onypko <vaporif@gmail.com>
2022-07-26 23:11:56 +0100

    dereference fix

--- src/ConferencePlanner/ConferencePlanner.Infrastructure/Meetings/MeetingsRepository.cs
+++ src/ConferencePlanner/ConferencePlanner.Infrastructure/Meetings/MeetingsRepository.cs
@@ -14,38 +14,38 @@ public class MeetingsRepository : IMeetingsRepository
 
     public async ValueTask<Meeting> AddAsync(Meeting entity, CancellationToken ct = default)
     {
-        var e = await _context.DbContext.AddAsync(entity, ct);
+        var e = await _context.DbContext!.AddAsync(entity, ct);
         return e!.Entity!;
     }
 
     public bool Delete(int id)
     {
-        var entity = _context.DbContext.Meetings.Find(id);
+        var entity = _context.DbContext!.Meetings.Find(id);
         if (entity is null)
         {
             return false;
         }
 
-        _context.DbContext.Meetings.Remove(entity);
+        _context.DbContext!.Meetings.Remove(entity);
         return true;
     }
 
     public async ValueTask<Meeting?> FindAsync(int id)
     {
-        var entity = await _context.DbContext.Meetings.FindAsync(id);
+        var entity = await _context.DbContext!.Meetings.FindAsync(id);
         if (entity is null)
         {
             return null;
         }
 
-        var entry = _context.DbContext.Entry(entity);
+        var entry = _context.DbContext!.Entry(entity);
         await entry.Collection(t => t.Participiants).LoadAsync();
         await entry.Reference(t => t.Organizer).LoadAsync();
 
         return entity;
     }
 
-    public IQueryable<Meeting> GetQueryable() => _context.DbContext.Meetings.AsNoTracking();
+    public IQueryable<Meeting> GetQueryable() => _context.DbContext!.Meetings.AsNoTracking();
 
     public async ValueTask SaveChangesAsync(CancellationToken ct = default)
     {
