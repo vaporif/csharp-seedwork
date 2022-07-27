ecc12907572716a21b36cac88ef4fa7664767704
Dmitriy Onypko <vaporif@gmail.com>
2022-07-27 03:42:36 +0100

    cleanup

--- src/SeedWork/SeedWork.DDD.EF/BoundedContext.cs
+++ src/SeedWork/SeedWork.DDD.EF/BoundedContext.cs
@@ -12,22 +12,17 @@ public class BoundedContext<T> : IAsyncDisposable, IDisposable
     private readonly MediatR.IPublisher _eventDispatcher;
     private readonly ICurrentUserProvider _currentUserProvider;
     private readonly IClock _clock;
-    private readonly List<EntityUpdates> Updates = new List<EntityUpdates>();
-
-    private readonly bool _trackChanges;
 
     public BoundedContext(
         IDbContextFactory<T> factory,
         MediatR.IPublisher eventDispatcher,
         ICurrentUserProvider currentUserProvider,
-        IClock clock,
-        bool trackChanges = true)
+        IClock clock)
     {
         DbContext = factory.CreateDbContext();
         _eventDispatcher = eventDispatcher ?? throw new ArgumentNullException(nameof(eventDispatcher));
         _currentUserProvider = currentUserProvider ?? throw new ArgumentNullException(nameof(currentUserProvider));
         _clock = clock ?? throw new ArgumentNullException(nameof(clock));
-        _trackChanges = trackChanges;
     }
 
     public async ValueTask<int> SaveChangesAsync(CancellationToken ct = default)
@@ -53,32 +48,9 @@ public class BoundedContext<T> : IAsyncDisposable, IDisposable
                     {
                         case EntityState.Added:
                             auditEntity.OnAdded(_clock.UtcNow, currentUserId);
-
-                            if (_trackChanges)
-                            {
-                                var changes = GetPropertyChanges(entry);
-                                Updates.Add(new EntityUpdates(entry.Entity));
-                            }
-
                             break;
                         case EntityState.Modified:
                             auditEntity.OnUpdated(_clock.UtcNow, currentUserId);
-
-                            if (_trackChanges)
-                            {
-                                var entityId = GetPrimaryKeyObject(auditEntity);
-                                var changes = GetPropertyChanges(entry);
-                                Updates.Add(new EntityUpdates(entry.Entity, entityId, changes));
-                            }
-
-                            break;
-                        case EntityState.Deleted:
-                            if (_trackChanges)
-                            {
-                                var entityId = GetPrimaryKeyObject(auditEntity);
-                                var changes = GetPropertyChanges(entry);
-                                Updates.Add(new EntityUpdates(entry.Entity, entityId));
-                            }
                             break;
                         default:
                             break;
@@ -130,37 +102,6 @@ public class BoundedContext<T> : IAsyncDisposable, IDisposable
 #pragma warning disable CA1816 // Dispose methods should call SuppressFinalize
         GC.SuppressFinalize(this);
 #pragma warning restore CA1816 // Dispose methods should call SuppressFinalize
-    }
-
-    private static UpdatedEntityPropertyChange[] GetPropertyChanges(EntityEntry entry)
-    {
-        return entry.Properties.Where(prop => prop.IsModified).Select(prop =>
-            new UpdatedEntityPropertyChange(prop.Metadata.Name, prop.CurrentValue, prop.OriginalValue)).ToArray();
-    }
-
-    private object GetPrimaryKeyObject(object entity)
-    {
-        var keyProps = GetPrimaryKeyProperties(entity).ToList();
-        var expandoKeyObject = new ExpandoObject();
-        var expandoCollection = (ICollection<KeyValuePair<string, object>>)expandoKeyObject!;
-
-        foreach (var x in keyProps)
-        {
-            expandoCollection.Add(new KeyValuePair<string, object>(x.Name, entity.GetPropertyValue(x.Name)));
-        }
-
-        return expandoKeyObject;
-    }
-
-    private object[] GetPrimaryKeys(object entity)
-    {
-        var keyProps = GetPrimaryKeyProperties(entity);
-        return keyProps.Select(x => entity.GetPropertyValue(x.Name)).ToArray();
-    }
-
-    private IList<IProperty> GetPrimaryKeyProperties(object entity)
-    {
-        return DbContext!.Model.FindEntityType(entity.GetType())?.FindPrimaryKey()?.Properties.ToList()!;
     }
 
     private void Dispose(bool disposing)
--- src/SeedWork/SeedWork.DDD.EF/EntityChangeState.cs
+++
@@ -1,6 +0,0 @@
-public enum EntityChangeState
-{
-    Add,
-    Update,
-    Delete
-}
